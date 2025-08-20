bdd0207a5688062762562ffb283141da990f013f
Clark Jones <clarkj338@gmail.com>
2025-08-20 18:16:31 +0530

    Update chat.ts

--- server/chat.ts
+++ server/chat.ts
@@ -692,11 +692,6 @@ export class CommandContext extends MessageContext {
 // Impulse Exp
 		if (this.user.registered) Impulse.ExpSystem.addExp(this.user.id, 1);
 
-		// emote test 
-		if (this.user.registered && message.includes(':')) {
-			message = require('../server/chat-plugins/impulse/emotes').parseEmotes(message, this.user.id);
-	}
-
 		this.update();
 
 		return message;
