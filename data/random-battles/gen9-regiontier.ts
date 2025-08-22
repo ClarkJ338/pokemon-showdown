21aa2745de9dcd192fcb8a3b69a2c4cb9cd507a8
Clark Jones <clarkj338@gmail.com>
2025-08-22 18:23:28 +0530

    Add Teams

--- data/random-battles/gen9-regiontier.ts
+++ data/random-battles/gen9-regiontier.ts
@@ -1,31 +1,83 @@
+/**
+ * Random Region vs Region Teams (OU, UU, RU) - Gen 9
+ * Author: Musddik's custom format
+ */
+
+import {RandomTeams} from '../random-teams';
+
+type RegionName = keyof typeof regionPools['ou'];
+
+const regionPools = {
+	ou: {
+		Kanto: ['Pikachu', 'Charizard', 'Gengar', 'Snorlax', 'Dragonite', 'Alakazam'],
+		Johto: ['Tyranitar', 'Suicune', 'Scizor', 'Ampharos', 'Heracross', 'Blissey'],
+		Hoenn: ['Metagross', 'Salamence', 'Gardevoir', 'Swampert', 'Milotic', 'Flygon'],
+		Sinnoh: ['Garchomp', 'Lucario', 'Togekiss', 'Infernape', 'Roserade', 'Gliscor'],
+		Unova: ['Hydreigon', 'Excadrill', 'Chandelure', 'Conkeldurr', 'Volcarona', 'Ferrothorn'],
+		Kalos: ['Greninja', 'Talonflame', 'Aegislash', 'Goodra', 'Noivern', 'Sylveon'],
+		Alola: ['Toucannon', 'Incineroar', 'Primarina', 'Kommo-o', 'Tsareena', 'Mudsdale'],
+		Galar: ['Dragapult', 'Corviknight', 'Cinderace', 'Toxtricity', 'Grimmsnarl', 'Duraludon'],
+		Paldea: ['Skeledirge', 'Pawmot', 'Annihilape', 'Baxcalibur', 'Garganacl', 'Iron Valiant'],
+	},
+	uu: {
+		Kanto: ['Arcanine', 'Machamp', 'Lapras', 'Rhydon', 'Victreebel', 'Clefable'],
+		Johto: ['Crobat', 'Kingdra', 'Miltank', 'Houndoom', 'Espeon', 'Umbreon'],
+		Hoenn: ['Ludicolo', 'Aggron', 'Manectric', 'Claydol', 'Walrein', 'Hariyama'],
+		Sinnoh: ['Electivire', 'Magmortar', 'Mamoswine', 'Bronzong', 'Spiritomb', 'Yanmega'],
+		Unova: ['Galvantula', 'Cofagrigus', 'Mienshao', 'Krookodile', 'Reuniclus', 'Jellicent'],
+		Kalos: ['Barbaracle', 'Malamar', 'Heliolisk', 'Trevenant', 'Gourgeist', 'Dragalge'],
+		Alola: ['Ribombee', 'Bewear', 'Palossand', 'Togedemaru', 'Drampa', 'Oricorio'],
+		Galar: ['Coalossal', 'Centiskorch', 'Indeedee', 'Frosmoth', 'Runerigus', 'Perrserker'],
+		Paldea: ['Kilowattrel', 'Bombirdier', 'Revavroom', 'Glimmora', 'Farigiraf', 'Maushold'],
+	},
+	ru: {
+		Kanto: ['Fearow', 'Dugtrio', 'Persian', 'Hitmonlee', 'Electrode', 'Tangela'],
+		Johto: ['Ariados', 'Lanturn', 'Quagsire', 'Granbull', 'Shuckle', 'Magcargo'],
+		Hoenn: ['Swellow', 'Cacturne', 'Whiscash', 'Torkoal', 'Banette', 'Glalie'],
+		Sinnoh: ['Purugly', 'Lopunny', 'Carnivine', 'Chatot', 'Bastiodon', 'Wormadam'],
+		Unova: ['Stoutland', 'Swoobat', 'Leavanny', 'Maractus', 'Alomomola', 'Beheeyem'],
+		Kalos: ['Furfrou', 'Meowstic', 'Pangoro', 'Aurorus', 'Carbink', 'Slurpuff'],
+		Alola: ['Gumshoos', 'Crabominable', 'Lurantis', 'Shiinotic', 'Bruxish', 'Komala'],
+		Galar: ['Greedent', 'Thievul', 'Dubwool', 'Boltund', 'Cramorant', 'Eiscue'],
+		Paldea: ['Squawkabilly', 'Oinkologne', 'Spidops', 'Bellibolt', 'Dachsbun', 'Grafaiai'],
+	},
+};
+
 export class RandomRegionTierTeams extends RandomTeams {
-  randomTeam(side: 'p1' | 'p2') {
-    const team = [];
+	getRegionPoolForTier(tier: 'ou' | 'uu' | 'ru') {
+		return regionPools[tier];
+	}
 
-    // Determine tier from format ID
-    let tier: 'ou' | 'uu' | 'ru' = 'ou';
-    if (this.format.id.includes('uu')) tier = 'uu';
-    if (this.format.id.includes('ru')) tier = 'ru';
+	randomRegion(): RegionName {
+		const tier = (this.format.id.includes('uu') ? 'uu' :
+			this.format.id.includes('ru') ? 'ru' : 'ou') as 'ou' | 'uu' | 'ru';
+		const pool = this.getRegionPoolForTier(tier);
+		return this.sample(Object.keys(pool)) as RegionName;
+	}
 
-    // Pick regions once per battle
-    if (!this.battle.regionMatchup) {
-      const allRegions = Object.keys(regionPools[tier]);
-      const p1Region = this.sample(allRegions);
-      const p2Region = this.sample(allRegions.filter(r => r !== p1Region));
-      this.battle.regionMatchup = {p1: p1Region, p2: p2Region};
-      this.battle.add(`message|${tier.toUpperCase()} Region Matchup: ${p1Region.toUpperCase()} vs ${p2Region.toUpperCase()}`);
-    }
+	randomTeam(): PokemonSet[] {
+		// Pick two distinct regions
+		const region1 = this.randomRegion();
+		let region2 = this.randomRegion();
+		while (region2 === region1) {
+			region2 = this.randomRegion();
+		}
 
-    const region = side === 'p1'
-      ? this.battle.regionMatchup.p1
-      : this.battle.regionMatchup.p2;
+		const tier = (this.format.id.includes('uu') ? 'uu' :
+			this.format.id.includes('ru') ? 'ru' : 'ou') as 'ou' | 'uu' | 'ru';
+		const pool = this.getRegionPoolForTier(tier);
 
-    const pool = this.sampleMany(regionPools[tier][region], 6);
+		// Merge both region pools
+		const combinedPool = [...pool[region1], ...pool[region2]];
 
-    for (const species of pool) {
-      team.push(this.randomSet(species)); // full competitive set
-    }
+		// Pick 6 Pokémon from the combined pool
+		const team: PokemonSet[] = [];
+		const chosenMons = this.sampleMany(combinedPool, 6);
 
-    return team;
-  }
-}
+		for (const name of chosenMons) {
+			team.push(this.randomSet(name));
+		}
+
+		return team;
+	}
+		}
